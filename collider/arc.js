551b29758ddc0f015243070a772a76d7e6a0d6d4
BrickNumber5 <79104486+BrickNumber5@users.noreply.github.com>
2021-02-16 17:56:32 -0700

    Update arc.js

--- collider/arc.js
+++ collider/arc.js
@@ -14,7 +14,9 @@ class ArcCollider extends Collider {
     arc( this.x, this.y, this.d, this.d, this.sA, this.eA )
   }
   collision( player, tx, ty ) {
-    
+    if ( ( this.x - player.x ) ** 2 + ( this.y - player.y ) ** 2 < ( ( this.d + playerWidth ) / 2 ) ** 2 ) return { collided: false }
+    let pta = atan2( player.y - this.y, player.x - this.x )
+    if ( !withinAngle( pta, this.sA, this.eA ) ) return { collided: false }
     let ins = lineSegmentCircleIntersection(
       player.x, player.y, tx, ty,
       this.x, this.y, ( this.d + playerWidth ) / 2
